664019bfedf365f1e23f63064d6097a222b31d93
Kjartan <kjartismarti@gmail.com>
2022-11-22 14:16:08 +0100

    added pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import {Outlet} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+        <Outlet/>
     </div>
   );
 }
---
+++ src/Pages/Pets.js
@@ -0,0 +1,5 @@
+const Pets = () => {
+    return ( <h2>This is the pets page</h2> );
+}
+ 
+export default Pets;
