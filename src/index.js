f66a2faa6e3695149253866eff5da33aa12bf863
Kjartan <kjartismarti@gmail.com>
2022-11-22 14:03:50 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<App/>}>
+    <Route path="/index" element={<Home/>}/>
+    <Route path="/pets" element={<Pets/>}/>
+
+
+  </Route>
+))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
